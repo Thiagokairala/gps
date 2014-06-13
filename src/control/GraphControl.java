a0adf7b0b0d6e17dcc81c430c9eb5295dfaefebb
Thiagokairala <thiagor.kairala@gmail.com>
2014-06-13 11:38:50 -0300

    Created Graph control to start creating djikstra

---
+++ src/control/GraphControl.java
@@ -0,0 +1,15 @@
+package control;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Edge;
+import model.Node;
+
+public class GraphControl {
+	public List<Edge> findSmallerPath(final Node nodeFrom, final Node nodeTo) {
+		List<Edge> explored = new ArrayList<Edge>();
+
+		return null;
+	}
+}
